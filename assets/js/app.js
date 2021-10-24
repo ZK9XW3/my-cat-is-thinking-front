a404e3c8907870836f572832443db80b0150fe0e
ZK9XW3 <peteralexandremusic@gmail.com>
2021-10-24 15:36:43 +0200

    feature(app.js): added axios + get thought text from API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,55 @@
+
 // DOM elements
 let thoughtButton = document.getElementById('thought-Button');
 let contentP = document.querySelector('.main__content__p');
 let contentImg = document.querySelector('.main__content__img');
 
-// Data into DOM
-let thought = "new cat thought";
-let imgSrc = './assets/img/' + 'hungry' + '.png';
+// imgUrlKeyword array
 
-// EventListener click put = modify DOM
+
+// EventListener modifies DOM when thought-button is clicked
 thoughtButton.addEventListener("click", e => {
-    contentP.innerHTML = thought;
-    contentImg.setAttribute('src', imgSrc);
+    
+    //append to DOM
+    const appendToDOM = (thought, imgUrlKeyword) => {
+        contentP.innerHTML = thought;
+        let imgUrl = './assets/img/' + imgUrlKeyword + '.png';
+        contentImg.setAttribute('src', imgUrl)
+    };
+    
+    //random number for thought's Id
+    const randomId = (maxId) => {
+        return Math.floor(Math.random() * maxId);
+    }
+
+    let randomIdResult = randomId(10);
+    console.log(randomIdResult);
+
+    // API request GET passing Id 
+    const fetchThought = () => {
+        axios.get('http://127.0.0.1:8000/api/thoughts/' + randomIdResult)
+            .then(response => {
+                const thought = response.data.text;
+
+                appendToDOM(thought, 'hungry');
+            })
+            .catch(error => console.error(error));
+    };
+
+    fetchThought();
+
+    // axios.get('http://127.0.0.1:8000/api/thoughts/2')
+    // .then(function (response) {
+    //     console.log('data requested');
+    //     const responseThought = response.data.text;
+    //     console.log(responseThought);
+    //     return responseThought;
+    // });
+    // console.log(responseThought);
+    // contentP.innerHTML = responseThought;
+    // contentImg.setAttribute('src', imgSrc);
 });
+
+// console.log(responseThought);
+
+
